ee97ab78ff059f3507e12e043fc40ceb0bce48e7
Nicholas Dalhaug <nicholasdalhaug@gmail.com>
2022-12-19 15:27:42 +0100

    Make install possible to run multiple times

--- README.md
+++ README.md
@@ -1,13 +1,12 @@
 # My dotfiles
-Remove:
-* .gitconfig
-* Global user settings for vscode (read the script)
-
 Run this simple script
 ```bash
 ./install.bash
 ```
+
 It will
-* Append sourcing of this new bashrc file to the end of the .bashrc, or create a new symlink
+* Append sourcing of this new bashrc file to the end of the .bashrc
 * Create a symlink to this `.gitignore`
 * Create a symlink to this VSCode `settings.json`
+
+It should tell you what to do if it is not able to install. 
--- install.bash
+++ install.bash
@@ -3,15 +3,44 @@
 BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 # bash
-# If .bashrc exists
 if [ -f $HOME/.bashrc ]; then
-    echo -e "\n\n# Source bashrc from dotfiles\nsource ${BASEDIR}/bashrc" >> $HOME/.bashrc
+    if grep -q "Source bashrc from dotfiles" $HOME/.bashrc; then
+        echo "Bash: Already exists"
+    else
+        echo "Bash: Adding sourcing of bashrc to the existing .bashrc"
+        echo "" >> $HOME/.bashrc
+        echo "# Source bashrc from dotfiles" >> $HOME/.bashrc
+        echo "source ${BASEDIR}/bashrc" >> $HOME/.bashrc
+        echo "" >> $HOME/.bashrc
+    fi
 else
-    ln -s ${BASEDIR}/bashrc ~/.bashrc
+    echo "Bash: Please create a .bashrc"
 fi
 
 # git
-ln -s ${BASEDIR}/gitconfig ~/.gitconfig
+if [ -f $HOME/.gitconfig ]; then 
+    link=$(readlink -f "$HOME/.gitconfig")
+    if [ "$link" == "${BASEDIR}/gitconfig" ]; then
+        echo "Git: Already exists"
+    else
+        echo "Git: Please delete the existing .gitconfig to allow for the symlink"
+    fi
+else
+    echo "Git: Adding symling to gitconfig"
+    ln -s ${BASEDIR}/gitconfig ~/.gitconfig
+fi
 
 # vscode
-ln -s ${BASEDIR}/vscode_settings.json ~/.config/Code/User/settings.json
+loc_from=$HOME/.config/Code/User/settings.json
+loc_to=${BASEDIR}/vscode_settings.json
+if [ -f $loc_from ]; then
+    link=$(readlink -f "$loc_from")
+    if [ "$link" == "$loc_to" ]; then
+        echo "VSCode: Already exists"
+    else
+        echo "VSCode: Please delete the existing $loc_from to allow for the symlink"
+    fi
+else
+    echo "VSCode: Adding symlink to vscode settings"
+    ln -s $loc_to $loc_from
+fi
